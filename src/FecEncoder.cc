9d127185b109443b60161f75c2d9f7e030b092b5
Zhenqing Zha <2663219537@qq.com>
2024-04-27 13:17:41 +0000

    finished var length fec

--- src/FecEncoder.cc
+++ src/FecEncoder.cc
@@ -3,6 +3,7 @@
 #include "cm256/cm256.h"
 #include <iostream>
 #include <glog/logging.h>
+#include <arpa/inet.h>
 #include <memory>
 using namespace std;
 using namespace google;
@@ -19,6 +20,8 @@ struct Fec_info{
     int redundancy_cnt;
     std::string type;
 };
+
+
 Fec_info GetInfoAboutFEC(Fec_type t) {
     switch (t)
     {
@@ -35,9 +38,60 @@ Fec_info GetInfoAboutFEC(Fec_type t) {
 }
 
 struct Datagram{
-    uint16_t len;
-    uint8_t data[dataLen];
+    // uint16_t len;
+    uint16_t fec_index;
+    uint8_t data[dataLen]; // **前两个byte存放长度  2^16 =65536** , 网络传输时需要htons
+    Datagram () {}
+    Datagram(const uint8_t* buffer, uint16_t len) {
+        SetLen(len);
+        memcpy(GetData(true), buffer, len);
+    }
+    Datagram(const char* buffer, uint16_t len) {
+        SetLen(len);
+        memcpy(GetData(true), buffer, len);
+    }
+    static Datagram* HostToNetwork(Datagram* d) {
+        d->SetFecIndex(htons(d->GetFecIndex()));
+        d->SetLen(htons(d->GetLen()));
+        return d;
+    } 
+    static Datagram* NetworkToHost(Datagram* d) {
+        d->SetFecIndex(ntohs(d->GetFecIndex()));
+        d->SetLen(ntohs(d->GetLen()));
+        return d;
+    } 
+
+    inline uint16_t GetFecIndex() const {
+        return fec_index;
+    }
+    inline void SetFecIndex(uint16_t fec_index) {
+        this->fec_index = fec_index;
+    }
+    inline uint16_t GetLen() const {
+        return *(uint16_t*)(data);
+    }
+    inline void SetLen(uint16_t len) {
+        *(uint16_t*)(data) = len;
+    }
+    inline const uint8_t* GetData(bool real_data) const {
+        return (uint8_t*)data + (real_data ? 2 : 0);
+
+    }
+    inline uint8_t* GetData(bool real_data) {
+        return (uint8_t*)data + (real_data ? 2 : 0);
+    }
+
 };
+
+void PrintDatagram(Datagram* d) {
+    cout << "fec_index: " << static_cast<int>(d->fec_index) << endl;
+    cout << "len: " << d->GetLen() << endl;
+    cout << "content: ";
+    for (size_t i = 0; i < d->GetLen(); ++i) {
+        cout << ((char*)d->GetData(true))[i];
+    }
+    cout << endl << endl;;
+}
 
 class FecEncoder
 {
@@ -50,38 +104,48 @@ public:
             cout << "init error" << endl;
         }
     };
-    void Encode(std::vector<std::shared_ptr<Datagram>>& buffers, Fec_type type) {
+    void Encode(std::vector<Datagram*>& buffers, Fec_type type) {
         if (buffers.empty()) return;
         Fec_info info = GetInfoAboutFEC(type);
         DCHECK_EQ(buffers.size(), info.data_cnt) << "buffer size is not equal to fec_type.data_cnt";
         params.OriginalCount = buffers.size();
         params.RecoveryCount = info.redundancy_cnt;
-        params.BlockBytes = buffers.front()->len;
+        params.BlockBytes = dataLen;
         for (int i = 0; i < buffers.size(); ++i) {
             // buffers[i].fec_index = cm256_get_original_block_index(params, i);
-            blocks[i].Block = buffers.at(i)->data;
+            blocks[i].Block = buffers.at(i)->GetData(false);
             blocks[i].Index = cm256_get_original_block_index(params, i);
-            params.BlockBytes = max(params.BlockBytes, (int)buffers.at(i)->len);
+            buffers[i]->fec_index = blocks[i].Index;
+            // params.BlockBytes = max(params.BlockBytes, (int)buffers.at(i)->GetLen());
         }
-        uint8_t* recovery_data = new uint8_t[info.redundancy_cnt * params.BlockBytes];
+        // LOG(ERROR) << "hello";
+        uint8_t* recovery_data = new uint8_t[info.redundancy_cnt * (dataLen + 2)];
         cm256_encode(params, blocks, recovery_data);
+        LOG(ERROR) << "hello";
+        LOG(ERROR) << info.redundancy_cnt;
         for (int i = 0; i < info.redundancy_cnt; ++i) {
-            auto ptr_d = std::make_shared<Datagram>();
-            ptr_d -> len = params.BlockBytes;
-            // d.fec_index = cm256_get_recovery_block_index(params, i);
-            memcpy(ptr_d->data, recovery_data + i * params.BlockBytes, params.BlockBytes);
+            auto ptr_d = new Datagram;
+        // LOG(ERROR) << "hello";
+
+            ptr_d->fec_index = cm256_get_recovery_block_index(params, i);
+            memcpy(ptr_d->GetData(false), recovery_data + i * params.BlockBytes, params.BlockBytes);
             buffers.push_back(ptr_d);
         }
+        // LOG(ERROR) << "hello";
+
+        delete[] recovery_data;
         return;
     }
-    bool Decode(std::vector<std::shared_ptr<Datagram>>& buffers, Fec_type type) {
+    bool Decode(std::vector<Datagram*>& buffers, Fec_type type) {
         
         Fec_info info = GetInfoAboutFEC(type);
         DCHECK_GE(buffers.size(), info.data_cnt) << "buffer size is not greater or equal to fec_type.data_cnt";
-        params.BlockBytes = buffers.front()->len;
+        params.BlockBytes = dataLen;
         for (int i = 0; i < info.data_cnt; ++i) {
-            blocks[i].Block = buffers.at(i)->data;
+            blocks[i].Block = buffers.at(i)->GetData(false);
+            blocks[i].Index = buffers.at(i)->fec_index;
         } 
+        return cm256_decode(params, blocks);
     }
     ~FecEncoder() {};
 };
@@ -93,12 +157,47 @@ int main(int argc, char** argv) {
     google::InstallFailureSignalHandler();
     FecEncoder f;
 
-    vector<std::shared_ptr<Datagram>> buffer;;
-    for (int i = 0; i < 5; ++i) {
-        auto ptr = std::make_shared<Datagram>();
-        buffer.push_back(ptr);
-    }
+    // vector<std::shared_ptr<Datagram>> buffer;
+    // for (int i = 0; i < 5; ++i) {
+    //     auto ptr = std::make_shared<Datagram>();
+    //     buffer.push_back(ptr);
+    // }
+    // f.Encode(buffer, Fec_type::FEC_2_1);
+    const char* s1 = "hello, world";
+    Datagram d1(s1, strlen(s1));
+
+
+    // memcpy(d1.data, s1, strlen(s1));
+    // d1.SetLen(strlen(s1));
+
+    const char* s2 = "thank you";
+    Datagram d2(s2, strlen(s2));
+    // memcpy(d2.data, s2, strlen(s2));
+    // d2.len = strlen(s2);
+
+    vector<Datagram*> buffer;
+    buffer.emplace_back(&d1);
+    buffer.emplace_back(&d2);
+    LOG(ERROR) << buffer.size();
     f.Encode(buffer, Fec_type::FEC_2_1);
+    LOG(ERROR) << buffer.size();
+    // for (int i = 0; i < buffer.size(); ++i) {
+    //     PrintDatagram(buffer.at(i));
+    // }
+    // LOG(ERROR) << static_cast<int>(buffer[0]->fec_index);
+    // LOG(ERROR) << static_cast<int>(buffer[1]->fec_index);
+    // LOG(ERROR) << static_cast<int>(buffer[2]->fec_index);
+    vector<Datagram*> new_buffer{buffer[0], buffer[2]};
+    // for (int i = 0; i < new_buffer.size(); ++i) {
+    //     PrintDatagram(new_buffer.at(i));
+    // }
+    LOG(ERROR) << f.Decode(new_buffer, Fec_type::FEC_2_1);
+
+    for (int i = 0; i < new_buffer.size(); ++i) {
+        // new_buffer[i]->SetLen(buffer[i]->len;
+        PrintDatagram(new_buffer.at(i));
+    }
+
     // 设置日志文件存储位置和文件名前缀
     
     // 设置当日志级别超过某阈值时，程序终止执行
@@ -108,5 +207,6 @@ int main(int argc, char** argv) {
     // LOG(ERROR) << "This is an error message";
     // LOG(WARNING) << "This is a warning message";
     // f.Encode({});
+    
 
 }

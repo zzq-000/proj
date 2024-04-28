139a0010413e59225000e7f4653e1deaedac8e88
Zhenqing Zha <2663219537@qq.com>
2024-04-28 05:01:22 +0000

    changed Datagram's api

--- src/FecEncoder.cc
+++ src/FecEncoder.cc
@@ -37,58 +37,98 @@ Fec_info GetInfoAboutFEC(Fec_type t) {
     }
 }
 
-struct Datagram{
+/*
+-------------
+|fec_index  |  
+---------------------------            为了节省fec开销，每次编解码时，不以最大长度dataLen作为params.BlockBytes, 
+|payload_len|   uint_16    ^           而是包组里面最大的 payload_len + sizeof(uint_16)
+-------------             encode 
+|           |             region       为了支持变长编解码，需要将payload_len也放在恢复区域，这样数据恢复出来之后可以
+|  payload  |                          知道有效数据的真实长度是怎样的
+|           |             v
+---------------------------
+
+*/
+
+class Datagram{
     // uint16_t len;
-    uint16_t fec_index;
-    uint8_t data[dataLen]; // **前两个byte存放长度  2^16 =65536** , 网络传输时需要htons
+private:
+    uint8_t fec_index;
+    uint8_t data[dataLen]; // ** 前两个byte存放长度  2^16 =65536 ** , 网络传输时需要htons
+public:
     Datagram () {}
-    Datagram(const uint8_t* buffer, uint16_t len) {
-        SetLen(len);
-        memcpy(GetData(true), buffer, len);
+    // Datagram(const uint8_t* payload, uint16_t len) {
+    //     SetPayloadLen(len);
+    //     SetPayload(payload, len);
+    // }
+    template<typename  T>
+    Datagram(const T* payload, uint16_t len) {
+        DCHECK_GE(dataLen - sizeof(uint16_t), len) << "pay load too large, should be less than dataLen - sizeof(uint16_t)";
+        SetPayloadLen(len);
+        SetPayload(payload, len);
     }
-    Datagram(const char* buffer, uint16_t len) {
-        SetLen(len);
-        memcpy(GetData(true), buffer, len);
+
+    inline uint8_t GetFecIndex() const {
+        return fec_index;
+    }
+    inline void SetFecIndex(uint8_t fec_index) {
+        this->fec_index = fec_index;
+    }
+
+    inline uint16_t GetPayloadLen() const {
+        return *(uint16_t*)(data);
+    }
+    inline void SetPayloadLen(uint16_t len) {
+        *(uint16_t*)(data) = len;
+    }
+    inline void SetPayload(const void* payload, uint16_t len) {
+        DCHECK_GE(dataLen - sizeof(uint16_t) /* exclude payload_len */, len) << "pay load too large, should be less than dataLen - sizeof(uint16_t)";
+        memcpy(GetPayload(), payload, len);
+    }
+    //返回的是携带的真实数据, payload
+    inline const uint8_t* GetPayload() const {
+        return (uint8_t*)data + sizeof(uint16_t);
+
+    }
+    inline uint8_t* GetPayload() {
+        return (uint8_t*)data + sizeof(uint16_t);
+    }
+
+
+    inline uint16_t GetCodecLen() const {
+        return GetPayloadLen() + sizeof(uint16_t); // use 2 bytes to indicated payload length
+    }
+    
+    inline void SetCodecData(const void* codec_data, uint16_t len) {
+        DCHECK_GE(dataLen, len) << "codec data too large, should be less than dataLen";
+        memcpy(GetCodecData(), codec_data, len);
+    }
+
+    // 返回的是需要fec处理的区域, payload_len + payload
+    inline const uint8_t* GetCodecData() const {
+        return (uint8_t*)data;
+
+    }
+    inline uint8_t* GetCodecData() {
+        return (uint8_t*)data;
     }
     static Datagram* HostToNetwork(Datagram* d) {
-        d->SetFecIndex(htons(d->GetFecIndex()));
-        d->SetLen(htons(d->GetLen()));
+        d->SetPayloadLen(htons(d->GetPayloadLen()));
         return d;
     } 
     static Datagram* NetworkToHost(Datagram* d) {
-        d->SetFecIndex(ntohs(d->GetFecIndex()));
-        d->SetLen(ntohs(d->GetLen()));
+        d->SetPayloadLen(ntohs(d->GetPayloadLen()));
         return d;
     } 
-
-    inline uint16_t GetFecIndex() const {
-        return fec_index;
-    }
-    inline void SetFecIndex(uint16_t fec_index) {
-        this->fec_index = fec_index;
-    }
-    inline uint16_t GetLen() const {
-        return *(uint16_t*)(data);
-    }
-    inline void SetLen(uint16_t len) {
-        *(uint16_t*)(data) = len;
-    }
-    inline const uint8_t* GetData(bool real_data) const {
-        return (uint8_t*)data + (real_data ? 2 : 0);
-
-    }
-    inline uint8_t* GetData(bool real_data) {
-        return (uint8_t*)data + (real_data ? 2 : 0);
-    }
 
 };
 
 void PrintDatagram(Datagram* d) {
-    cout << "fec_index: " << static_cast<int>(d->fec_index) << endl;
-    cout << "len: " << d->GetLen() << endl;
+    cout << "fec_index: " << static_cast<int>(d->GetFecIndex()) << endl;
+    cout << "len: " << d->GetPayloadLen() << endl;
     cout << "content: ";
-    for (size_t i = 0; i < d->GetLen(); ++i) {
-        cout << ((char*)d->GetData(true))[i];
+    for (size_t i = 0; i < d->GetPayloadLen(); ++i) {
+        cout << ((char*)d->GetPayload())[i];
     }
     cout << endl << endl;;
 }
@@ -110,28 +150,28 @@ public:
         DCHECK_EQ(buffers.size(), info.data_cnt) << "buffer size is not equal to fec_type.data_cnt";
         params.OriginalCount = buffers.size();
         params.RecoveryCount = info.redundancy_cnt;
-        params.BlockBytes = dataLen;
+        params.BlockBytes = buffers.front()->GetCodecLen();
         for (int i = 0; i < buffers.size(); ++i) {
             // buffers[i].fec_index = cm256_get_original_block_index(params, i);
-            blocks[i].Block = buffers.at(i)->GetData(false);
+            blocks[i].Block = buffers.at(i)->GetCodecData();
             blocks[i].Index = cm256_get_original_block_index(params, i);
-            buffers[i]->fec_index = blocks[i].Index;
-            // params.BlockBytes = max(params.BlockBytes, (int)buffers.at(i)->GetLen());
+            buffers[i]->SetFecIndex(blocks[i].Index);
+            params.BlockBytes = max(params.BlockBytes, (int)buffers.at(i)->GetCodecLen());
         }
-        // LOG(ERROR) << "hello";
-        uint8_t* recovery_data = new uint8_t[info.redundancy_cnt * (dataLen + 2)];
+
+        uint8_t* recovery_data = new uint8_t[info.redundancy_cnt * params.BlockBytes];
         cm256_encode(params, blocks, recovery_data);
-        LOG(ERROR) << "hello";
+
         LOG(ERROR) << info.redundancy_cnt;
         for (int i = 0; i < info.redundancy_cnt; ++i) {
             auto ptr_d = new Datagram;
-        // LOG(ERROR) << "hello";
 
-            ptr_d->fec_index = cm256_get_recovery_block_index(params, i);
-            memcpy(ptr_d->GetData(false), recovery_data + i * params.BlockBytes, params.BlockBytes);
+
+            ptr_d->SetFecIndex(cm256_get_recovery_block_index(params, i));
+            ptr_d->SetCodecData(recovery_data + i * params.BlockBytes, params.BlockBytes);
             buffers.push_back(ptr_d);
         }
-        // LOG(ERROR) << "hello";
+
 
         delete[] recovery_data;
         return;
@@ -139,11 +179,14 @@ public:
     bool Decode(std::vector<Datagram*>& buffers, Fec_type type) {
         
         Fec_info info = GetInfoAboutFEC(type);
-        DCHECK_GE(buffers.size(), info.data_cnt) << "buffer size is not greater or equal to fec_type.data_cnt";
+        if (buffers.size() < info.data_cnt) {
+            return false;
+        }
+        // DCHECK_GE(buffers.size(), info.data_cnt) << "buffer size is not greater or equal to fec_type.data_cnt";
         params.BlockBytes = dataLen;
         for (int i = 0; i < info.data_cnt; ++i) {
-            blocks[i].Block = buffers.at(i)->GetData(false);
-            blocks[i].Index = buffers.at(i)->fec_index;
+            blocks[i].Block = buffers.at(i)->GetCodecData();
+            blocks[i].Index = buffers.at(i)->GetFecIndex();
         } 
         return cm256_decode(params, blocks);
     }
@@ -155,14 +198,9 @@ int main(int argc, char** argv) {
     google::SetLogDestination(google::INFO, "./");
     google::SetLogFilenameExtension("log_");
     google::InstallFailureSignalHandler();
+
     FecEncoder f;
 
-    // vector<std::shared_ptr<Datagram>> buffer;
-    // for (int i = 0; i < 5; ++i) {
-    //     auto ptr = std::make_shared<Datagram>();
-    //     buffer.push_back(ptr);
-    // }
-    // f.Encode(buffer, Fec_type::FEC_2_1);
     const char* s1 = "hello, world";
     Datagram d1(s1, strlen(s1));
 
@@ -178,35 +216,17 @@ int main(int argc, char** argv) {
     vector<Datagram*> buffer;
     buffer.emplace_back(&d1);
     buffer.emplace_back(&d2);
-    LOG(ERROR) << buffer.size();
+    // LOG(ERROR) << buffer.size();
     f.Encode(buffer, Fec_type::FEC_2_1);
-    LOG(ERROR) << buffer.size();
-    // for (int i = 0; i < buffer.size(); ++i) {
-    //     PrintDatagram(buffer.at(i));
-    // }
-    // LOG(ERROR) << static_cast<int>(buffer[0]->fec_index);
-    // LOG(ERROR) << static_cast<int>(buffer[1]->fec_index);
-    // LOG(ERROR) << static_cast<int>(buffer[2]->fec_index);
+
     vector<Datagram*> new_buffer{buffer[0], buffer[2]};
-    // for (int i = 0; i < new_buffer.size(); ++i) {
-    //     PrintDatagram(new_buffer.at(i));
-    // }
+
     LOG(ERROR) << f.Decode(new_buffer, Fec_type::FEC_2_1);
 
     for (int i = 0; i < new_buffer.size(); ++i) {
         // new_buffer[i]->SetLen(buffer[i]->len;
         PrintDatagram(new_buffer.at(i));
     }
-
-    // 设置日志文件存储位置和文件名前缀
-    
-    // 设置当日志级别超过某阈值时，程序终止执行
-    // google::SETLEve
-    // LOG(INFO) << "This is an info message";
-    // LOG(WARNING) << "This is a warning message";
-    // LOG(ERROR) << "This is an error message";
-    // LOG(WARNING) << "This is a warning message";
-    // f.Encode({});
     
 
 }

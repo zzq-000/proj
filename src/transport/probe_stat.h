e8633ed6859cbc245cfa42bffd304ea167557c86
Zhenqing Zha <2663219537@qq.com>
2024-05-13 07:27:58 +0000

    add LossStat

--- src/transport/probe_stat.h
+++ src/transport/probe_stat.h
@@ -66,3 +66,60 @@ class ProbeStat{
         return {rtt_mean, rtt_std};
     }
 };
+
+class LossStat{
+    struct Record{
+        uint64_t second;
+        int raw_packet_count;
+        int raw_packet_loss_count;
+        int data_packet_count;
+        int data_packet_loss_count;
+    };
+    const static int kRecordLen = 60;
+    Record data[kRecordLen];
+
+    LossStat() {
+        memset(data, 0, sizeof(data));
+    }
+
+    void RegisterAPacket(uint64_t ts, bool received, bool processed, bool is_redundant) {
+        uint64_t second = ts / 1000;
+        int index = second % kRecordLen;
+        if (data[index].second != second) {
+            data[index].second = second;
+            data[index].raw_packet_count = 0;
+            data[index].raw_packet_loss_count = 0;
+            data[index].data_packet_count = 0;
+            data[index].data_packet_loss_count = 0;
+        }
+        data[index].raw_packet_count++;
+        data[index].raw_packet_loss_count += (received ? 0 : 1);
+        if (!is_redundant) { // Todo
+            data[index].data_packet_count++;
+            data[index].data_packet_loss_count += (processed ? 0 : 1);
+        }
+    }
+    
+    std::pair<double, double> GetLastMiniteLossRate(uint64_t ts) const {
+        uint64_t start_second = ts / 1000 - 60;
+        uint64_t end_second = start_second + 59;
+
+        int total_raw_packet_count = 0;
+        int total_raw_packet_loss_count = 0;
+        int total_data_packet_count = 0;
+        int total_data_packet_loss_count = 0;
+
+        for (int i = 0; i < kRecordLen; ++i) {
+            if (data[i].second >= start_second && data[i].second <= end_second) {
+                total_raw_packet_count += data[i].raw_packet_count;
+                total_raw_packet_loss_count += data[i].raw_packet_loss_count;
+                total_data_packet_count += data[i].data_packet_count;
+                total_data_packet_loss_count += data[i].data_packet_loss_count;
+            }
+        }
+        double raw_packet_loss = (double)total_raw_packet_loss_count / total_raw_packet_count;
+        double data_packet_loss = (double)total_data_packet_loss_count / total_data_packet_count;
+        return {raw_packet_loss, data_packet_loss};
+    }
+
+};
